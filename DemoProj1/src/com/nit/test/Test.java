32061ae0d11c8542f967d9adb44608fc1b2ad34e
tl <tl@gamil.com>
2020-05-25 08:19:22 +0530

    Main Method Modified

--- DemoProj1/src/com/nit/test/Test.java
+++ DemoProj1/src/com/nit/test/Test.java
@@ -1,7 +1,9 @@
 package com.nit.test;
 
-public class Test {
-public static void main(String[] args) {
-	
-}
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+	}
 }
